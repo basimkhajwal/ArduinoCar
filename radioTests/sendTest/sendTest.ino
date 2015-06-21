15c7bccf7b93d40c6f781a284e78663701384db3
Basim Khajwal <basimkhajwal@gmail.com>
2015-06-21 19:21:16 +0100

    Test program for the sender

---
+++ radioTests/sendTest/sendTest.ino
@@ -0,0 +1,15 @@
+#define RF_PIN 3
+
+void setup() {
+  pinMode(RF_PIN, OUTPUT);
+}
+
+void loop() {
+  for (int i = 4000; i > 20; i -= i / 3) {
+    digitalWrite(RF_PIN, HIGH);
+    delay(2000);
+     
+     digitalWrite(RF_PIN,LOW);
+     delay(i); 
+  }
+}
