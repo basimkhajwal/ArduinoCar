e2cb31c3108ad1542b3b317b42289db7fa7fbfdc
Basim Khajwal <basimkhajwal@gmail.com>
2015-09-13 18:53:14 +0100

    Finished receiver code - now for some testing

--- radio/reciever/reciever.ino
+++ radio/reciever/reciever.ino
@@ -2,6 +2,12 @@
 
 #define RF_PIN 3
 
+#define LEFT_PIN 6
+#define RIGHT_PIN 5
+
+#define FORWARD_PIN 11
+#define BACK_PIN 10
+
 /*
 
 A valid instruction follows the following format
@@ -9,17 +15,36 @@ A valid instruction follows the following format
 1EDDDSSS
 
 Where E is enable, if 0 then all motors are off
-      D is direction, 000 is North and the rest anti-clockwise compass directions
-      S is speed, 000 being off and 111 is full speed
+      D is direction, (see below)
+      S is speed, 000 being off and 111 is full speed (TO BE DONE LATER)
+
+Directions:
+0 - Forward
+1 - Forward Left
+2 - Forward Right
+3 - Backward
+4 - Backward Left
+5 - Backward Right
+
+Any other value will default to Forward
 
 */
 const uint8_t VALID_MASK = 0x80;
 const uint8_t ENABLE_MASK = 0x40;
+const uint8_t DIRECTION_MASK = 0x38;
+const uint8_t SPEED_MASK = 0x07;
 
 uint8_t state;
 boolean dirty = false;
 
 void setup() {
+  pinMode(LEFT_PIN, OUTPUT);
+  pinMode(RIGHT_PIN, OUTPUT);
+  pinMode(FORWARD_PIN, OUTPUT);
+  pinMode(BACK_PIN, OUTPUT);
+  state = 0;
+  parseState();
+  
   vw_set_rx_pin(RF_PIN);
   vw_setup(500);
   
@@ -28,8 +53,49 @@ void setup() {
 
 void parseState() {
   
+  boolean disabled = (ENABLE_MASK & state) == 0;
+  int carDirection = disabled ? 0 : (DIRECTION_MASK & state) >> 3;
+  int carSpeed = disabled ? 0 : SPEED_MASK & state;
   
+  digitalWrite(LEFT_PIN, LOW);
+  digitalWrite(RIGHT_PIN, LOW);
+  digitalWrite(FORWARD_PIN, LOW);
+  digitalWrite(BACK_PIN, LOW);
   
+  //Later on do PWM for now it is normal on-off no speed
+  if (carSpeed != 0) {
+    switch (carDirection) {
+    
+      case 1:
+        digitalWrite(FORWARD_PIN, HIGH);
+        digitalWrite(LEFT_PIN, HIGH);
+        break;
+    
+      case 2:
+        digitalWrite(FORWARD_PIN, HIGH);
+        digitalWrite(RIGHT_PIN, HIGH);
+        break;
+    
+      case 3:
+        digitalWrite(BACK_PIN, HIGH);
+        break;
+        
+      case 4:
+        digitalWrite(BACK_PIN, HIGH);
+        digitalWrite(LEFT_PIN, HIGH);
+        break;
+        
+      case 5:
+        digitalWrite(BACK_PIN, HIGH);
+        digitalWrite(RIGHT_PIN, HIGH);
+        break;
+        
+      default:
+        digitalWrite(FORWARD_PIN, HIGH);
+        break;
+    
+    }
+  }
 }
 
 void loop() {
